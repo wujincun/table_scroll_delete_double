86c2d7c2cb73b3868e22d2105f06f0a21449f430
wujincun <1769453779@qq.com>
2016-11-11 17:48:21 +0800

    first

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2016/11/11.
+ */
+$(function () {
+    var $title = $('.title');
+    var $content = $('content');
+    var $contentLine = $content.find('tr');
+    $contentLine.each(function (key,value) {
+        var $thisCol = $(value).find('td');
+        $thisCol.each(function (key,vule) {
+            var thisText = $()
+        })
+
+    })
+})
