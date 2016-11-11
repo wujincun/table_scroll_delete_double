48460bc6f8255d8be6eece19cc4e8ede5606bfd3
wujincun <1769453779@qq.com>
2016-11-11 18:04:22 +0800

    first

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@
  */
 $(function () {
     var $title = $('.title');
+    var $titleLine = $title.find('tr');
     var $content = $('content');
     var $contentLine = $content.find('tr');
     $contentLine.each(function (key,value) {
-        var $thisCol = $(value).find('td');
+        var $thisLineN = $(value).find('td');
         $thisCol.each(function (key,vule) {
-            var thisText = $()
-        })
+
+        });
 
     })
 })
