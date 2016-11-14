cdbf30aa7988ef6c9b9fbea99e4f81b2a6cecb1b
wujincun <1769453779@qq.com>
2016-11-14 09:21:08 +0800

    second

--- js/main.js
+++ js/main.js
@@ -4,12 +4,32 @@
 $(function () {
     var $title = $('.title');
     var $titleLine = $title.find('tr');
-    var $content = $('content');
+    var $content = $('.content');
     var $contentLine = $content.find('tr');
+    var flagObj = {};
     $contentLine.each(function (key,value) {
+        var lineObj = {};
+        flagObj[key] = true;
         var $thisLineN = $(value).find('td');
-        $thisLineN.each(function (key,vule) {
+        if($thisLineN.length != 0){
+            $thisLineN.each(function (index,numValue) {
+                var thisNum = Number($(numValue).text());
+                if(index == 0){
+                    lineObj[0] = thisNum;
+                }else{
+                    if(lineObj[0] != thisNum){
+                        flagObj[key] = false;
+                        return true
+                    }
+                }
+            });
+        }else{
+            flagObj[key] = false;
+        }
+        if(flagObj[key] == true){
+            $contentLine.eq(key).addClass('showHide');
+            $titleLine.eq(key).addClass('showHide')
+        }
+    });
 
-        });
-    })
-})
+});
